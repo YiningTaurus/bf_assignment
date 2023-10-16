3faf7d7119b97a0b9b1f87f3d5aea7c0e9ab60de
YiningTaurus <yiningtaurus@gmail.com>
2023-10-16 14:14:41 -0700

    add little sheep

--- src/main/java/week1day1/HelloWorld.java
+++ src/main/java/week1day1/HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         System.out.println("My name is Yining Xu. I like playing computer games.");
+        System.out.println("Little sheep is smart!");
     }
 }
